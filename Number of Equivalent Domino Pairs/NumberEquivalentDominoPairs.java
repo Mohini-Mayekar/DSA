2582c71e9fdf0b6c38fc313411623cccc1d7d4b8
Mohini Mayekar <mmayekar97@gmail.com>
2025-05-04 13:33:44 -0400

    Number of Equivalent Domino Pairs

---
+++ Number of Equivalent Domino Pairs/NumberEquivalentDominoPairs.java
@@ -0,0 +1,19 @@
+//TC: O(n);
+//SC: O(1);
+//n - length of dominoes array
+
+class Solution {
+
+    public int numEquivDominoPairs(int[][] dominoes) {
+        int[] num = new int[100];
+        int ret = 0;
+        for (int[] domino : dominoes) {
+            int val = domino[0] < domino[1]
+                ? domino[0] * 10 + domino[1]
+                : domino[1] * 10 + domino[0];
+            ret += num[val];
+            num[val]++;
+        }
+        return ret;
+    }
+}

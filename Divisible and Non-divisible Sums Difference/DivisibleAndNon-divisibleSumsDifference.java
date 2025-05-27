fc77e26188a2c2eedb115be0f13d6a5c7140dbfc
Mohini Mayekar <mmayekar97@gmail.com>
2025-05-27 14:39:03 -0400

    Divisible and Non-divisible Sums Difference

---
+++ Divisible and Non-divisible Sums Difference/DivisibleAndNon-divisibleSumsDifference.java
@@ -0,0 +1,17 @@
+//TC: O(n)
+//SC: O(1)
+
+class Solution {
+    public int differenceOfSums(int n, int m) {
+        int res = 0;
+
+        for(int i = 1; i <= n; i++){
+            if(i % m !=0 ){
+                res += i;
+            } else {
+                res -= i;
+            }
+        }
+        return res;
+    }
+}
